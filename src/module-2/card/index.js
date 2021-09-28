87d44ad60bc7b50e3c12a8e29301b6a38fe7697b
Daryna <cherniuk.daryna@gmail.com>
2021-09-28 19:33:12 +0300

    done module 2

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -21,7 +21,50 @@ export default class Card {
     this.render();
   }
 
-  render () {
-    // ... your logic
+  getTemplate () {
+    return `<div class="product-card">
+      <div class="image">
+          <img class="image__goods" src="${this.images[0]}" />
+      </div>
+
+      <div class="rating-price">
+          <div class="rating">
+          <span>${this.rating}</span>
+              <img class="rating__image-star" src="img/star.svg"/>
+          </div>
+          
+          <p class="price">${this.price}</p>
+      </div>
+                          
+      <p class="product-card__name">${this.brand}</p>
+      <p class="product-card__information">${this.title}</p>
+
+      <div class="product-card-buttons">
+          <button class="product-card-buttons__wishlist">
+            <img class="product-card-buttons__image-heart" src="img/heart.svg"/>
+            <span class="product-card-buttons__name">WISHLIST</span>
+          </button><button class="product-card-buttons__add">
+            <img class="product-card-buttons__image-basket" src="img/basket.svg"/>
+            <span class="product-card-buttons__name">ADD TO CART</span>
+          </button>
+      </div> 
+    </div>`
+  }
+
+  render(){
+    const productCardWrapper = document.createElement('div');
+    productCardWrapper.innerHTML = this.getTemplate();
+    this.element = productCardWrapper.firstElementChild;
+  }
+
+  remove(){
+    if(this.element){
+      this.element.remove();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
   }
 }
