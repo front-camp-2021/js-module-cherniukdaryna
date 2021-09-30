84bab2040cf0aecd7ccd7fe37b80c0821d7f6120
Daryna <cherniuk.daryna@gmail.com>
2021-09-30 18:50:48 +0300

    created render

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,9 +1,33 @@
 import Card from '../../module-2/card/index.js';
 
 export default class CardsList {
-  constructor (data = []) {
+  constructor ({data = [], Component = {}}) {
     this.data = data;
+    this.Component = Component;
 
-    // ... your logic
+    this.render();
   }
+
+  render () {
+    const productCardsList = document.createElement("div");
+    productCardsList.classList.add("catalog");
+    this.element = productCardsList;
+
+    for (const elem of this.data){
+      const card  = new this.Component(elem);
+      this.element.append(card.element);
+    }
+  }
+
+  remove () {
+    if(this.element){
+      this.element.remove();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+  }
+  
 }
