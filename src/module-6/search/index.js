edf34042cfe9e99ad60baa55a6b2220dc7a57e78
cherniukdaryna <cherniuk.daryna@gmail.com>
2021-12-10 18:24:02 +0200

    add search

--- src/module-6/search/index.js
+++ src/module-6/search/index.js
@@ -1,5 +1,72 @@
 import { debounce } from '../../module-1/debounce/index.js';
 
 export default class Search {
+  element;
 
+  constructor() {
+    this.value = '';
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="search">
+        <input class="search__input" type="search" id="search" placeholder="  Search"/>
+
+        <button class="search__btn-glass">
+            <img class="search__image-search" src="img/4801315_glass_magnifier_magnifying_search_searching_icon.svg"/>
+        </button>
+       </div>`
+  }
+
+  render() {
+    const search = document.createElement('div');
+    search.innerHTML = this.getTemplate();
+    this.element = search;
+
+    this.addEvent()
+  }
+
+  debounce = (fn, delay = 0) => {
+    let timeout;
+    return function(){
+      const functionCall = () => {fn.apply(this, arguments)};
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(functionCall, delay);
+    };
+  }
+
+  dispatchEvent (string) {
+    this.element.dispatchEvent(
+      new CustomEvent(
+        'search-filter',
+        {
+          bubbles: true,
+          detail: string
+        })
+    )
+  }
+
+  addEvent () {
+    this.element.addEventListener('keyup', event => {
+      if (event.target.className === 'search__input'){
+        this.value = event.target.value;
+
+        this.dispatchEvent(this.value);
+      }
+    });
+  }
+
+  remove () {
+    if(this.element){
+      this.element.remove();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+  }
 }
